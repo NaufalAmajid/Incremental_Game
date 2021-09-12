5bacc6d273dbac126a3538d7b0241f0a1a45bf1e
NaufalAmajid <naufalamajid@gmail.com>
2021-09-12 13:35:42 +0700

    last commit

---
+++ Assets/Script/ResourceController.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public Button ResourceButton;
+    public Image ResourceImage;
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    //SOUND EFFECT
+    public AudioSource myfx;
+    public AudioClip click;
+    public AudioClip click_upgrade;
+
+    private ResourceConfig _config;
+
+    private int _level = 1;
+
+    public bool IsUnlocked { get; private set; }
+
+    //PRIVATE VOID START
+    private void Start ()
+    {
+        ResourceButton.onClick.AddListener (() =>
+        {
+            if (IsUnlocked)
+            {
+                UpgradeLevel ();
+            }
+            else
+            {
+                UnlockResource ();
+            }
+        });
+        
+    }
+
+    //VOID SETCONFIG
+    public void SetConfig (ResourceConfig config)
+    {
+        _config = config;
+
+        
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        ResourceUnlockCost.text = $"Unlock Cost\n{ _config.UnlockCost }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
+
+        SetUnlocked (_config.UnlockCost == 0);
+        
+        
+    }
+
+    //PUBLIC DOUBLE GETOUTPUT
+    public double GetOutput ()
+    {
+        return _config.Output * _level;
+    }
+
+    //PUBLIC DOUBLE GETUPGRADECOST
+    public double GetUpgradeCost ()
+    {
+        return _config.UpgradeCost * _level;
+    }
+
+    //PUBLIC DOUBLE GETUNLOCKCOST
+    public double GetUnlockCost ()
+    {
+        return _config.UnlockCost;
+        
+    }
+
+    // VOID UPGRADELEVEL
+    public void UpgradeLevel ()
+    {
+        double upgradeCost = GetUpgradeCost ();
+        if (GameManager.Instance.TotalGold < upgradeCost)
+        {
+            return;
+        }
+
+        GameManager.Instance.AddGold (-upgradeCost);
+        _level++;
+
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+
+        myfx.PlayOneShot(click_upgrade);
+   }
+
+    //VOID UNLOCKRESOURCE
+    public void UnlockResource ()
+    {
+        double unlockCost = GetUnlockCost ();
+        if (GameManager.Instance.TotalGold < unlockCost)
+        {
+            return;
+        }
+
+        SetUnlocked (true);
+        GameManager.Instance.ShowNextResource ();
+        AchievementController.Instance.UnlockAchievement (AchievementType.UnlockResource, _config.Name);
+        
+        myfx.PlayOneShot(click_upgrade);
+    }
+
+    //VOID SETUNLOCKED
+    public void SetUnlocked (bool unlocked)
+    {
+        IsUnlocked = unlocked;
+        ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
+        ResourceUnlockCost.gameObject.SetActive (!unlocked);
+        ResourceUpgradeCost.gameObject.SetActive (unlocked);
+        
+    }
+}
---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+ 
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{   
+    //SOUND EFFECT
+    public AudioSource myFX;
+    public AudioClip click;
+
+    //VOID CLICKSOUND
+    public void ClickSound()
+    {
+
+        myFX.PlayOneShot(click);
+    }
+
+    //VOID ONPOINTERDOWN
+    public void OnPointerDown (PointerEventData eventData)
+    {
+
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+
+    }
+
+}   
